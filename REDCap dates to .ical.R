19fa5e6ec5a822402ce99dbea4c9eef44a67e654
agdamsbo <agdamsbo@pm.me>
2021-05-06 11:05:27 +0200

    minor update

--- REDCap dates to .ical.R
+++ REDCap dates to .ical.R
@@ -4,7 +4,10 @@ source("convert_ical.R")
 library(calendar)
 
 # Formatting
-df<-date_file_prep(folder="/Users/andreas/REDCap_conversion/calendar")
+f_path<-"/Users/andreas/REDCap_conversion/calendar"
+df<-date_file_prep(folder=f_path,include_all=FALSE,cut_date=-5,num_c=1,ev_names=c("3mdr"),col_spec="_book3")
+
+## Room variable is set as fixed value, change when REDCap is updated.
 
 # Conversion
 ic_write(convert_ical(start=df$value,id=df$id,event=df$name,room=df$room)[[2]], "enigma_control_all.ics")
