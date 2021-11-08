e292973bd8c4a728c2e2e7033d47d3a0113dc7a1
AG Damsbo <agdamsbo@pm.me>
2021-11-08 11:57:50 +0100

    Inclusion status plot

---
+++ src/status_plot.R
@@ -0,0 +1,25 @@
+# Visuals
+library(lubridate)
+
+d <- d[with(d, order(record_id)), ]
+
+trend_line<-data.frame(number=c(1,as.numeric(difftime(d$incl_date[nrow(d)],d$incl_date[1],units = "weeks"))),
+                       date=c(d$incl_date[1],d$incl_date[nrow(d)]))
+
+p1<-ggplot(data = d, aes(x = incl_date, y = record_id))+
+  geom_line(color = "#00AFBB", size = 2)+
+  scale_x_date(date_breaks = "1 month",date_labels = "%b/%Y")+
+  xlab("Inclusion date")+
+  ylab("Number of included patients")+
+  geom_line(data=trend_line,aes(x=date,y=number))
+
+
+dt<-d[is.na(d$eos2),]
+dt$non_eeos<-as.numeric(seq(dt$record_id))
+
+p2<-ggplot(data = dt, aes(x = incl_date, y = non_eeos))+
+  geom_line(color = "#00AFBB", size = 2)+
+  scale_x_date(date_breaks = "1 month",date_labels = "%b/%Y")+
+  xlab("Inclusion date")+
+  ylab("Number of included patients")+
+  geom_line(data=trend_line,aes(x=date,y=number))
