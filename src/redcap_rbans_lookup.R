df042eb031ad2bc79300897713f7cd03a8c90f83
AG Damsbo <agdamsbo@pm.me>
2022-02-24 14:36:44 +0100

    Splitting CIs to lower/upper for easier later analysis

--- src/redcap_rbans_lookup.R
+++ src/redcap_rbans_lookup.R
@@ -22,4 +22,33 @@ for (i in sel1){
   ## Using the dplyr::if_else for a more stringent vectorisation
 }
 
-ds<-replace(df, TRUE, lapply(df, type.convert)) ## Converting types
+## Spliting CIs in lower and upper
+loups<-c()
+for (i in 1:nrow(df)){
+  # i=34
+  cis<-c()
+  for (j in colnames(select(df,ends_with("_ci")))){
+    # j="rbans_a_ci"
+    cis<-c(cis,unlist(strsplit(df[i,j],split="[-]")))
+  }
+  loups<-c(loups,c(df$record_id[i],cis))
+}
+loups<-data.frame(matrix(loups,ncol = 13,byrow = TRUE))
+
+### Naming and merging
+cnms<-c()
+for (k in colnames(select(df,ends_with("_ci")))){
+  # j="rbans_a_ci"
+  stp<-unlist(strsplit(k,split="[_]"))[-3]
+  cnms<-c(cnms,paste0(paste(stp,collapse = "_"),"_",c("lo","up")))
+}
+
+colnames(loups)<-c("record_id",cnms)
+
+loups$record_id<-as.numeric(loups$record_id)
+
+df<-merge(df,loups)
+
+## Type conversion
+csel<-colnames(select(df,!ends_with("_ci"))) ## Not including "_ci" as these should stay as character
+df[,csel]<-replace(df[,csel], TRUE, lapply(df[,csel], type.convert)) ## Converting types
