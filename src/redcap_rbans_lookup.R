76051ba48e6ec82035d548fb397d42f708f57b0c
AG Damsbo <agdamsbo@pm.me>
2022-02-24 13:29:00 +0100

    Type conversion for data upload

--- src/redcap_rbans_lookup.R
+++ src/redcap_rbans_lookup.R
@@ -12,3 +12,13 @@ df<-cbind(index_from_raw(dta = select(dta,c("record_id",ends_with("_rs"))),
                          raw_columns=names(select(dta,ends_with("_rs")))),redcap_event_name=dta$redcap_event_name)
 
 colnames(df)<-c("record_id",colnames(cbind(select(dta,ends_with("_is")),select(dta,ends_with("_ci")),select(dta,ends_with("_per")))),"redcap_event_name")
+
+sel1<-colnames(select(df,ends_with("_per")))
+for (i in sel1){
+  df[,i]<-if_else(df[,i]=="> 99.9","99.95",
+                 if_else(df[,i] =="< 0.1", "0.05",
+                                  df[,i]))
+  ## Using the dplyr::if_else for a more stringent vectorisation
+}
+
+ds<-replace(df, TRUE, lapply(df, type.convert)) ## Converting types
--- src/redcap_upload_rbans3.R
+++ src/redcap_upload_rbans3.R
@@ -1,11 +1,5 @@
 ## RBANS calculations upload
-
-# Index loading
-index<-read.csv("/Users/au301842/ENIGMAtrial_R/data/index.csv")
-
-# RBANS index conversion
-source("https://raw.githubusercontent.com/agdamsbo/ENIGMAtrial_R/main/src/index_from_raw.R")
-
+### Pre-reading
 records_mod <- redcap_read_oneshot(
   redcap_uri   = uri,
   token        = token,
@@ -15,7 +9,7 @@ records_mod <- redcap_read_oneshot(
   filter(is.na(visit_data_mod)) %>% ## Only write to patients not already filled
   select(record_id) ## Keeping record_id to select for download
 
-
+### Data export
 dta <- redcap_read(
   redcap_uri   = uri,
   token        = token,
@@ -29,17 +23,11 @@ dta <- redcap_read(
 
 ## Handling only 3 months by script also for 12 months handling
 
-df<-cbind(index_from_raw(dta = select(dta,c("record_id",ends_with("_rs"))),
-                   table=index,
-                   version = dta$urbans_version,
-                   age=dta$rbans_age,
-                   raw_columns=names(select(dta,ends_with("_rs")))),redcap_event_name=dta$redcap_event_name)
-
-colnames(df)<-c("record_id",colnames(cbind(select(dta,ends_with("_is")),select(dta,ends_with("_ci")),select(dta,ends_with("_per")))),"redcap_event_name")
+source("https://raw.githubusercontent.com/agdamsbo/ENIGMAtrial_R/main/src/redcap_rbans_lookup.R")
 
 ## Write
 
-# stts<-redcap_write(ds=df,
-#                    redcap_uri   = uri,
-#                    token        = token)
+stts<-redcap_write(ds=df,
+                   redcap_uri   = uri,
+                   token        = token)
 
