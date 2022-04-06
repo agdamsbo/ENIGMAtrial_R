a9961eb7c7a90deebe84e332308999a5e409337f
AG Damsbo <agdamsbo@pm.me>
2022-04-06 14:21:45 +0200

    Complete revision

--- scripts/RBANS index lookup.R
+++ scripts/RBANS index lookup.R
@@ -1,27 +1,36 @@
+## =============================================================================
 # RBANS Index Lookup and visualisation
-
-## This forces wide data, but should be kept as long and indexed by event name
-
-inc<-c("record_id","age")
-mn3<-c("record_id","urbans_version","rbans_a_rs","rbans_b_rs","rbans_c_rs","rbans_d_rs","rbans_e_rs")
-
-field_list<-list(inc,
-                 mn3)
+## =============================================================================
 
 
-names(field_list)<-c("inclusion_arm_1",
-                     "3_months_arm_1")
+## =============================================================================
+## Settings
+## =============================================================================
 
-id   = "1"
+id         = 40
 
-suppressWarnings(source("src/rbans_index_prep.R"))
+event      = "3_months_arm_1"
 
-# - NAs are just thrown out.
+## =============================================================================
+## Data export
+## =============================================================================
 
-## Now includes all relevant data. Maybe include event in df? This would enable the analysis of change in score. Requires different plotting.
+## Use "redcap_api_export.R" function instead??
+
+source("src/redcap_api_export_short.R")
+
+df <- df %>%
+  select(c("record_id",ends_with(c("_is","_lo","_up","_per")))) %>%
+  na.omit()
+
+## Now includes all relevant data. Maybe include event in df? This would enable the analysis of change in score. Requires different plotting
+
+## =============================================================================
+## Plots
+## =============================================================================
 
 source("src/rbans_plot.R")
 
-grid.arrange(index_plot, percentile_plot, nrow=2, ncol=1)
+index_plot/percentile_plot ## Patchwork syntax
 
-## Use kable package for beautiful table
+
