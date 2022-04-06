78964e8ab006b74d2dd23645ccbd8f17bd14db9e
AG Damsbo <agdamsbo@pm.me>
2022-04-06 14:22:24 +0200

    Revision, misses CIs

--- src/rbans_plot.R
+++ src/rbans_plot.R
@@ -1,50 +1,53 @@
-## Revision needed!
+## Revision under way.
+## CIs still pending.
 
-library("reshape2")
-library("ggplot2")
-library("gridExtra")
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(patchwork)
 
 # Melting df as long format
-df_long <- melt(df, id = "id")
+df_long <- df %>% pivot_longer(cols=-record_id)
 
 # Set x-axis names
 domain_names<-c("immediate","visuospatial","verbal","attention","delayed","total")
 
 # Define specific dataframes
 df_index <- df_long %>%
-  filter(grepl('index',variable)) %>%
-  mutate(value=as.numeric(value)) %>%
-  mutate(variable=factor(variable,labels = domain_names))
+  filter(grepl('_is',name)) %>%
+  mutate(value=as.numeric(value),
+         name=factor(name,labels = domain_names)) 
 
-df_X95pct <- df_long %>%
-  filter(grepl('X95pct',variable))
 
-low<-c()
-hgh<-c()
-for (i in 1:nrow(df_X95pct)){
-  low<-c(low,unlist(strsplit(df_X95pct$value[i],"[-]"))[1])
-  hgh<-c(hgh,unlist(strsplit(df_X95pct$value[i],"[-]"))[2])
-}
-df_index<-data.frame(df_index,low=as.numeric(low),high=as.numeric(hgh))
+# df_X95pct <- df_long %>%
+#   filter(grepl('X95pct',variable))
+# 
+# low<-c()
+# hgh<-c()
+# for (i in 1:nrow(df_X95pct)){
+#   low<-c(low,unlist(strsplit(df_X95pct$value[i],"[-]"))[1])
+#   hgh<-c(hgh,unlist(strsplit(df_X95pct$value[i],"[-]"))[2])
+# }
+# df_index<-data.frame(df_index,low=as.numeric(low),high=as.numeric(hgh))
 
 # Correcting odd percentile formatting
 # Wonder if this can be done a little more elegantly??
-sel1<-grepl("percentile", df_long$variable)
-# [i,"value"]
-df_long[sel1,"value"]<-ifelse(df_long[sel1,"value"] %in% c("> 99.9",">99.9"),"99.95",
-                              ifelse(df_long[sel1,"value"] %in% c("< 0.1","<0.1"), "0.05", 
-                                     ifelse(df_long[sel1,"value"]%in% c("0,1"),"0.1",
-                                            df_long[sel1,"value"])))
+# sel1<-grepl("percentile", df_long$variable)
+# # [i,"value"]
+# df_long[sel1,"value"]<-ifelse(df_long[sel1,"value"] %in% c("> 99.9",">99.9"),"99.95",
+#                               ifelse(df_long[sel1,"value"] %in% c("< 0.1","<0.1"), "0.05", 
+#                                      ifelse(df_long[sel1,"value"]%in% c("0,1"),"0.1",
+#                                             df_long[sel1,"value"])))
 
 # Percentile dataframe
 df_percentile <- df_long %>%
-  filter(grepl('percentile',variable)) %>%
-  # mutate(value=as.numeric(value)) %>%
-  mutate(variable=factor(variable,labels = domain_names))
+  filter(grepl('_per',name)) %>%
+  mutate(value=as.numeric(value),
+         name=factor(name,labels = domain_names)) 
 
 
 # Plotting index scores
-index_plot<-ggplot(data=df_index, aes(x=variable, y=value, color=factor(id), group=factor(id))) + 
+index_plot<-ggplot(data=df_index, aes(x=name, y=value, color=factor(record_id), group=factor(record_id))) + 
   geom_point() +
   # geom_errorbar(width=0.05,aes(ymin = low,
   #                   ymax = high)) + 
@@ -59,7 +62,7 @@ index_plot<-ggplot(data=df_index, aes(x=variable, y=value, color=factor(id), gro
         axis.ticks.x=element_blank())
 
 # Plotting percentiles
-percentile_plot<-ggplot(data=df_percentile, aes(x=variable, y=as.numeric(value), fill=factor(id)))+
+percentile_plot<-ggplot(data=df_percentile, aes(x=name, y=value, fill=factor(record_id)))+
   geom_col(position = "dodge") +
   expand_limits(y=c(0,100)) +
   scale_y_continuous(breaks=seq(0,100,by=10)) +
@@ -67,9 +70,6 @@ percentile_plot<-ggplot(data=df_percentile, aes(x=variable, y=as.numeric(value),
   ylab("Percentile") + 
   # geom_hline(yintercept=50) + # Expected average
   labs(fill = "ID")
-
-# index_plot                                 # Draw ggplot2 plot
-# percentile_plot
 
 # Clean-up
 source("src/remove_all_but.R")
