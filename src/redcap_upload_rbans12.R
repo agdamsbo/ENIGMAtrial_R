ae7911c707f3da00ab0395aa1a4120669b19535d
AG Damsbo <agdamsbo@pm.me>
2022-04-08 09:24:43 +0200

    Major revision and conclusion included

--- src/redcap_upload_rbans12.R
+++ src/redcap_upload_rbans12.R
@@ -1,14 +1,23 @@
 ## RBANS calculations upload
-### Pre-reading
+
+## =============================================================================
+## Step 1: Pre-reading
+## =============================================================================
+
 records_mod <- redcap_read_oneshot(
   redcap_uri   = uri,
   token        = token,
-  events       = "12_months_arm_1",
   fields       = c("record_id","eos_data_mod","rbans_perf") ## Only selecting relevant variables
 )$data %>%
-  # filter(is.na(eos_data_mod)) %>% ## Only write to patients not already filled
-  filter(rbans_perf==1) %>% ## The two filters are kept seperated for troubleshooting
+  filter(is.na(eos_data_mod)) %>% ## Only write to patients not already filled
+  filter(rbans_perf==1&redcap_event_name=="12_months_arm_1") %>% ## The two filters are kept seperated for troubleshooting
   select(record_id) ## Keeping record_id to select for download
+
+## =============================================================================
+## Step 2: Doing table look-ups for RBANS incl upload
+## =============================================================================
+
+# Everything is wrapped within an "if" loop for 
 
 if (length(records_mod[[1]])>0){
 ### Data export
@@ -18,7 +27,7 @@ dta <- redcap_read(
   events       = "12_months_arm_1",
   raw_or_label = "raw",
   records      = records_mod[[1]],
-  forms        = c("rbans","rbans_konklusion"),
+  forms        = c("rbans","rbans_konklusion","mrs"),
   fields       = "record_id"
 )$data
   
@@ -27,13 +36,116 @@ dta <- redcap_read(
 
 source("https://raw.githubusercontent.com/agdamsbo/ENIGMAtrial_R/main/src/redcap_rbans_lookup.R")
 
-## Composing standard conclusion text
-
-# source("https://raw.githubusercontent.com/agdamsbo/ENIGMAtrial_R/main/src/redcap_rbans_conclusions.R")
-
+## Last minute flag to indicate modification performed
+df<-df%>%full_join(.,data.frame(record_id=.$record_id,
+                            redcap_event_name="end_of_study_arm_1",
+                            eos_data_mod="yes"),
+               by=c("record_id","redcap_event_name"))
 
 ## Write
-stts<-redcap_write(ds=df%>%mutate(eos_data_mod="yes"), ## Last minute flag to indicate modification performed
+stts<-redcap_write(ds=df,
                    redcap_uri   = uri,
                    token        = token)
+
+
+## =============================================================================
+## Step 3: Conclusions data
+## =============================================================================
+
+## Common variables for 3 + 12 months
+cols<-colnames(select(df,c("record_id",ends_with(c("_is","_per","name")))))
+
+rb12<-df%>%
+  select(all_of(cols))%>%
+  left_join(.,dta%>%
+              select(c("record_id","mrs_score")),by="record_id")%>%
+  filter(redcap_event_name=="12_months_arm_1")
+
+rb3<-redcap_read(
+  redcap_uri   = uri,
+  token        = token,
+  events       = "3_months_arm_1",
+  raw_or_label = "raw",
+  records      = records_mod[[1]],
+  forms        = c("rbans","mrs"),
+  fields       = c("record_id")
+)$data%>%
+  select(all_of(cols),"mrs_score")
+
+## Prestroke mRS
+rb0<-redcap_read(
+  redcap_uri   = uri,
+  token        = token,
+  events       = "inclusion_arm_1",
+  raw_or_label = "raw",
+  records      = records_mod[[1]],
+  forms        = c("mrs","iqcode"),
+  fields       = c("record_id")
+)$data%>%
+  select("record_id","mrs_score","iq_score")
+
+## =============================================================================
+## Step 4: Conclusions text
+## =============================================================================
+
+doms<-c("immediate","visuospatial","verbal","attention","delayed","total")
+txts<-c()
+
+# Texts for each ID
+for (i in records_mod[[1]]) {
+  rb0_s<-rb0[rb0$record_id==i]
+  rb3_s<-rb3[rb3$record_id==i]
+  rb12_s<-rb12[rb12$record_id==i]
+  
+txt_mrs<-paste(c("Prestroke","One month","End of Study"),"mRS:",
+               c(rb0_s$mrs_score,rb3_s$mrs_score,rb12_s$mrs_score),
+               collapse = ", ")
+
+txt_iqcode<-paste("At inclusion, IQCODE score was:",
+                  rb0_s$iq_score,
+                  "(48 is normal, higher indicates decline)")
+
+txt_md3<-paste0("3 months RBANS index scores were ", 
+               paste(select(rb3_s,ends_with("_is")),collapse = ", "), 
+               ", for the domains respectively: ", 
+               paste(doms,collapse = ", "),
+               "; with percentiles: ",
+               paste(select(rb3_s,ends_with("_per")),collapse = ", ")
+               )
+
+txt_md12<-paste0("12 months RBANS index scores were ", 
+                paste(select(rb12_s,ends_with("_is")),collapse = ", "), 
+                ", for the domains respectively: ", 
+                paste(doms,collapse = ", "),
+                "; with percentiles: ",
+                paste(select(rb12_s,ends_with("_per")),collapse = ", ")
+                )
+
+if (rb12_s$mrs_score>=2 & rb12_s$rbans_i_is<=70) {
+  txt_demens<-"OBS: Pt opfylder muligvis kriterierne for demens"
+} else {
+  txt_demens<-"Pt opfylder ikke kriterierne for demens"}
+
+txt_note<-"NOTE: Index score at 70-85 is 'forringet', and <70 is 'reduceret'"
+
+txts<-c(txts,
+        paste0(paste(txt_mrs,txt_iqcode,txt_md3,txt_md12,txt_demens,txt_note,
+        sep=".\n"),
+        "."
+        ))
 }
+
+# Creating upload data frame
+ds<-data.frame(record_id=records_mod[[1]],
+               rbans_conc_text=txts,
+               redcap_event_name="12_months_arm_1")
+
+## =============================================================================
+## Step 5: Conclusions upload
+## =============================================================================
+
+stts<-redcap_write(ds=ds,
+                   redcap_uri   = uri,
+                   token        = token)
+
+} 
