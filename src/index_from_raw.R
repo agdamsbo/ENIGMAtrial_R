22529c09292374b8db1bd0d0c4af7ab56ded6459
AG Damsbo <agdamsbo@pm.me>
2022-08-18 13:45:26 +0200

    ad

--- src/index_from_raw.R
+++ src/index_from_raw.R
@@ -99,38 +99,7 @@ index_from_raw<-function(ds,indx,version,age,raw_columns,mani=FALSE){
                               df[,i]))
       ## Using the dplyr::if_else for a more stringent vectorisation
     }
-    
-    ## Spliting CIs in lower and upper
-    loups<-c()
-    for (i in 1:nrow(df)){
-      # i=34
-      cis<-c()
-      for (j in colnames(select(df,ends_with("_ci")))){
-        # j="rbans_a_ci"
-        cis<-c(cis,unlist(strsplit(df[i,j],split="[-]")))
-      }
-      loups<-c(loups,c(df$record_id[i],cis))
-    }
-    loups<-data.frame(matrix(loups,ncol = 13,byrow = TRUE))
-    
-    ### Naming and merging
-    cnms<-c()
-    for (k in colnames(select(df,ends_with("_ci")))){
-      # j="rbans_a_ci"
-      stp<-unlist(strsplit(k,split="[_]"))[-3]
-      cnms<-c(cnms,paste0(paste(stp,collapse = "_"),"_",c("lo","up")))
-    }
-    
-    df<-merge(df,loups %>%
-                'colnames<-'(c("record_id",cnms)) %>%
-                mutate(record_id=as.numeric(record_id)))
-    
-    ## Type conversion
-    
-    df<- df %>%
-      mutate(across(.cols=all_of(colnames(select(df,c("record_id",
-                                                      ends_with(c("_is","_per","_lo","_up")))))), ## Selecting variables to include, keeping "_ci" and event_name out.
-                    ~as.numeric(.))) ## Converting types
+
   }
   
   return(df)
