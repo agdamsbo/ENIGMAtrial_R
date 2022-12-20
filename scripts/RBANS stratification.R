b0f8e9bbe9f5e06accb9de906a88741523f09778
AG Damsbo <agdamsbo@pm.me>
2022-12-20 20:38:34 +0100

    new scripts

---
+++ scripts/RBANS stratification.R
@@ -0,0 +1,53 @@
+## =============================================================================
+# RBANS mean score stratification
+## =============================================================================
+
+## =============================================================================
+## Function
+## =============================================================================
+
+library(dplyr)
+
+## Nnew read_redcap_data function introduced, that splits all forms into separate data.frames in list.
+## Includes some clean-up.
+
+## =============================================================================
+## Data export
+## =============================================================================
+
+ls <- stRoke::read_redcap_data(uri   = "https://redcap.au.dk/api/",
+                      token= names(suppressWarnings(read.csv("/Users/au301842/enigma_redcap_token.csv",colClasses = "character"))),
+                      fields = c("class_toast","record_id"),
+                      forms= "rbans",
+                      events= c("3_months_arm_1",
+                                "12_months_arm_1",
+                                "end_of_study_arm_1"))
+
+df <- ls$rbans |> select(c("record_id",
+                     "redcap_event_name",
+                     ends_with("_is"))) |> 
+  mutate(redcap_event_name=factor(redcap_event_name,
+                                  levels = c("3_months_arm_1",
+                                             "12_months_arm_1"),
+                                  labels = c("3 måneder","12 måneder"))) |> 
+  left_join(ls$klassifikation_af_primre_stroke |> select(record_id,class_toast)) |> 
+  na.omit()
+
+## =============================================================================
+## Plots
+## =============================================================================
+
+# This should just be one pipe with facet.by=event
+
+source("src/plot_index.R")
+
+df |> select(-c(record_id)) |> group_by(class_toast,redcap_event_name) %>% 
+  summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>% 
+  mutate(class_toast=factor(class_toast,
+                                levels = c("1","2","3","5"),
+                                labels = c("Storkar","Småkar","Kardio", "Ukendt"))) %>%
+  plot_index(id="class_toast",scores = colnames(.)[-1],facet.by = "redcap_event_name")+
+  labs(title="Stratified mean RBANS scores",
+       colour="TOAST")
+
+
