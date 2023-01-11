f59650e999d492cd484e82b9b7f53e609f926311
AG Damsbo <agdamsbo@pm.me>
2023-01-11 14:37:59 +0100

    calendar update 2023-01-11 2023-01-11 14:37:59

--- src/convert_ical.R
+++ src/convert_ical.R
@@ -1,4 +1,4 @@
-convert_ical<-function(start,id,time=7200,name,room){
+convert_ical<-function(df,start,id,time=7200,name,room){
   library(calendar)
   # Package from here: https://github.com/ATFutures/calendar/
   library(lubridate)
@@ -6,10 +6,10 @@ convert_ical<-function(start,id,time=7200,name,room){
   # Standard event duration is 7200s=2h
   # Converts list of single event times to calendar entries for each ID with duration of standard length
   # Based on example from https://github.com/ATFutures/calendar/issues/36
-  fix <-  data.frame(SUMMARY = paste0("ID", id, " ",name),
-                     DTSTART = ymd_hm(start,tz="CET"),
-                     DTEND = ymd_hm(start,tz="CET")+time,
-                     LOCATION = room,
+  fix <-  data.frame(SUMMARY = paste0("ID", df[,id], " ",df[,name]),
+                     DTSTART = ymd_hm(df[,start],tz="CET"),
+                     DTEND = ymd_hm(df[,start],tz="CET")+time,
+                     LOCATION = df[,room],
                      stringsAsFactors = FALSE)
   fix<-fix %>% mutate(UID = replicate(nrow(fix), ic_guid()))
   ic <- ical(fix[!is.na(fix$DTSTART),])
